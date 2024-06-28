ac414ca42e99fc80c35c536e80d0b874b5ef1634
hbjamin <hbjamin@sas.upenn.edu>
2024-06-28 17:26:51 -0400

    cut out dark noise before pre pulsing peak and after late pulsing peak

--- generation/make_avg_timing_pdf.py
+++ generation/make_avg_timing_pdf.py
@@ -57,6 +57,12 @@ def subtractPedestal(time,prob,ped_start_time,ped_end_time):
 def gaussian(t,A,mu,sigma):
     return A*np.exp(-((t-mu)**2)/(2*sigma**2))
 
+def exponential_func(t,A,b,c):
+    return A*np.exp(b*(t-c))
+
+def mirrored_exponential_func(t,A,b,c):
+    return A*np.exp(-b*(t-c))
+
 def fitPeakToGaussian(time,prob,dt):
     max_i=getMaxIndex(prob)
     A_guess=prob[max_i]
@@ -97,7 +103,6 @@ def averageWithNearestNeighbors(time,prob,start_time,end_time,n):
             for j in range(2*n+1):
                 avg+=prob[i-n+j]/(2*n+1)
             new_prob.append(avg)
-            #new_prob.append((prob[i-7]+prob[i-6]+prob[i-5]+prob[i-4]+prob[i-3]+prob[i-2]+prob[i-1]+prob[i]+prob[i+1]+prob[i+2]+prob[i+3]+prob[i+4]+prob[i+5]+prob[i+6]+prob[i+7])/15)
             new_tot+=new_prob[-1]
         else:
             new_prob.append(prob[i])
@@ -107,6 +112,76 @@ def averageWithNearestNeighbors(time,prob,start_time,end_time,n):
             new_prob[i]=new_prob[i]*tot/new_tot
             final_tot+=new_prob[i]
     return new_prob
+
+
+# Find intersection of exponential fit of start of prompt peak and average
+# dark rate before prompt peak. Then remove everything before that 
+def removeNoiseBeforePromptPeak(time,prob):
+    # Get dark rate
+    dark_rate=n=0
+    for i in range(len(time)):
+        if time[i]<-10:
+            dark_rate+=prob[i]
+            n+=1
+    dark_rate=dark_rate/n
+    # Get data to fit 
+    fit_time=[]
+    fit_prob=[]
+    for i in range(len(time)):
+        if time[i]>=-3 and time[i]<=-1:
+            fit_time.append(time[i])
+            fit_prob.append(prob[i])
+    # Make initial guess of parameters and fit
+    guess=[0.005,0.5,-3] 
+    params,covariance=curve_fit(exponential_func,fit_time,fit_prob,guess)
+    y_fit=exponential_func(time,params[0],params[1],params[2]) 
+    plt.plot(time,y_fit,color='red')
+    x_line=np.linspace(-20,0,100)
+    y_line=[]
+    for i in range(len(x_line)):
+        y_line.append(dark_rate)
+    plt.plot(x_line,y_line,color='red')
+    # Find intersection
+    for i in range(len(time)):
+        if y_fit[i]>dark_rate:
+            # Remove everything before that 
+            print("crossing at",time[i])
+            return time[i:],prob[i:]
+
+# Find intersection of exponential fit of tail of late pulsing peak
+# and average dark rate after. Then remove everything after that
+# **** This is only done for 8" PMTs because of their weird problem ****
+def removeNoiseAfterLatePeak(time,prob):
+    # Get dark rate
+    dark_rate=n=0
+    for i in range(len(time)):
+        if time[i]>50 and time[i]<60:
+            dark_rate+=prob[i]
+            n+=1
+    dark_rate=dark_rate/n
+    # Get data to fit 
+    fit_time=[]
+    fit_prob=[]
+    for i in range(len(time)):
+        if time[i]>=37 and time[i]<=40:
+            fit_time.append(time[i])
+            fit_prob.append(prob[i])
+    # Make initial guess of parameters and fit
+    guess=[0.005,0.1,35] 
+    params,covariance=curve_fit(mirrored_exponential_func,fit_time,fit_prob,guess)
+    y_fit=mirrored_exponential_func(time,params[0],params[1],params[2]) 
+    plt.plot(time,y_fit,color='red')
+    x_line=np.linspace(35,60,100)
+    y_line=[]
+    for i in range(len(x_line)):
+        y_line.append(dark_rate)
+    plt.plot(x_line,y_line,color='red')
+    # Find intersection
+    for i in range(len(time)):
+        if y_fit[-1-i]>dark_rate:
+            print("crossing at",time[i])
+            return time[:-1-i],prob[:-1-i]
+    return time,prob
 
 def main():
     
@@ -143,12 +218,18 @@ def main():
                 sigma=params[2]
                 sigma_err=errors[2]
                 max_index=getMaxIndex(counts)
+                
                 # Smooth out late pulsing (pulses after 5 sigma of prompt peak)
                 counts=averageWithNearestNeighbors(time,counts,time[max_index]+5*sigma,time[-10],7)       
+                
                 # Smooth out pre pulsing (pulses before 3 sigma of promt peak)
                 counts=averageWithNearestNeighbors(time,counts,time[10],time[max_index]-3*sigma,7)
-                # Subtract pedestal (average counts between 0 and 30 ns)
+                
+                # Subtract pedestal 
+                # At this point the window is -50 to ~150 ns
+                # 0 to 30 ns is always dark pulses for all runs of all pmt types
                 counts=subtractPedestal(time,counts,0,30)   
+                
                 # Center prompt peak at 0 ns
                 time,counts=recenter(counts,max_index,new_time_domain,dt)
                 ratio=getLatePulsing(time,counts,5*sigma)
@@ -168,6 +249,8 @@ def main():
     # For every pmt type
     for i in range(len(pmt_type)):  
         print('Average',pmt_type[i],'timing pdf')
+        # Normalize PDF
+        total_counts[i]=normalize(np.array(total_counts[i]),dt).tolist()
         # Fit the peak to a gaussian
         params,covariance=fitPeakToGaussian(total_time[i],total_counts[i],dt)
         errors=np.sqrt(np.diag(covariance))
@@ -177,32 +260,51 @@ def main():
         sigma_err=errors[2]
         print('Fit a TTS sigma of',round(sigma,4),'+/-',round(sigma_err,4))
         x_fit=np.linspace(mu-2*sigma,mu+2*sigma,100)
-        y_fit=gaussian(x_fit,A,mu,sigma)
-        plt.figure(int(i*2+1))
-        plt.plot(total_time[i][int(15/dt):int(25/dt)],total_counts[i][int(15/dt):int(25/dt)])
-        plt.plot(x_fit,y_fit,color='red')
-        plt.xlabel('deltat [ns]')
-        plt.ylabel('Pulses/ns')
-        plt.title(pmt_type[i])
-        # Normalize PDF
-        prob=normalize(total_counts[i],dt)
-        total=0
-        for j in range(len(prob)):
-            total+=prob[j]*dt
-        print('Sum of probability after normalization is:',total)
-        ratio=getLatePulsing(total_time[i],total_counts[i],5*sigma)
-        print('Late Ratio is:',100*ratio,'percent')
-        prob=prob.tolist()
-        time=total_time[i].tolist()
-        print('time :',time)
-        print('time_prob :',prob)
+        y_fit=gaussian(x_fit,A,mu,sigma) 
+        #plt.figure(int(i*2+1))
+        #plt.plot(total_time[i][int(15/dt):int(25/dt)],total_counts[i][int(15/dt):int(25/dt)])
+        #plt.plot(total_time[i],total_counts[i])
+        #plt.plot(x_fit,y_fit,color='red')
+        #plt.xlabel('deltat [ns]')
+        #plt.ylabel('Pulses/ns')
+        #plt.title(pmt_type[i])
+
+
+
+      
+
+        # Cut out all pulses after late pulsing
+        # See function definition for details
+        # See docdb documentation for reasoning
+
+        
         plt.figure(int(i*2+2))
-        plt.plot(time,prob)
+        plt.plot(total_time[i],total_counts[i],color='blue')
+        # Remove dark rate before prompt peak
+        time_new,prob_new=removeNoiseBeforePromptPeak(total_time[i],total_counts[i])
+        prob_new=normalize(np.array(prob_new),dt).tolist()
+        time_new=time_new.tolist()
+        if i==0:
+            # Remove dark rate after late peak 
+            time_new,prob_new=removeNoiseAfterLatePeak(time_new,prob_new)
+        plt.plot(time_new,prob_new,color='orange')
         plt.yscale('log')
+        plt.ylim(1e-6,1)
         plt.xlabel('Time [ns]')
         plt.ylabel('Probability')
         plt.xlim(-20,80)
         plt.grid()
+
+        total=0
+        for j in range(len(prob_new)):
+            total+=prob_new[j]*dt
+        print('Sum of probability after normalization is:',total)
+        ratio=getLatePulsing(time_new,prob_new,5*sigma)
+        print('Late Ratio is:',100*ratio,'percent')
+        #prob_new=prob_new.tolist()
+        #time_new=time_new.tolist()
+        print('time :',time_new)
+        print('time_prob :',prob_new)
     plt.show()
 
 if __name__ == '__main__':
